cb8124f0134e9d27fbe3870b51b32ad783392d2f
Konstantin S. <havendv@gmail.com>
2026-04-03 09:06:53 +0400

    feat: Updated OpenAPI spec (#9)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/LabelStudio/Generated/LabelStudio.ExportClient.Download.g.cs
+++ src/libs/LabelStudio/Generated/LabelStudio.ExportClient.Download.g.cs
@@ -144,7 +144,6 @@ namespace LabelStudio
                 try
                 {
                     __response.EnsureSuccessStatusCode();
-
                     var __content = await __response.Content.ReadAsByteArrayAsync(
 #if NET5_0_OR_GREATER
                         cancellationToken
